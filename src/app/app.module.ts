be52c1e6820b4152336cfe96aa1f76e29b29dc97
Shane <shane.donnelly12666@gmail.com>
2022-12-06 20:44:27 +0000

    Updated app.component.html so that it now displays a custom header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./layout/header/header.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NbLayoutModule, NbThemeModule, NbUserModule} from "@nebular/theme";
+import {NbEvaIconsModule} from "@nebular/eva-icons";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'corporate'}),
+    NbEvaIconsModule,
+    NbUserModule,
+    HttpClientModule,
+    NbLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
